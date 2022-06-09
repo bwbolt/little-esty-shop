c983191a485ea92f78d06d95f6472e842fee671d
Bryce Wein <bryce.wein@gmail.com>
2022-06-09 19:56:08 -0400

    Feat: add merchant bulk discounts view controller and routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,13 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :merchants do
+    get '/bulk_discounts', to: 'merchant_bulk_discounts#index'
+    get '/bulk_discounts/:id', to: 'merchant_bulk_discounts#show'
     resources :items, only: %i[index show edit update new create]
     resources :dashboard, only: [:index]
     resources :invoices, only: %i[index show] do
       resources :invoice_items, only: %i[edit update]
     end
-    # resources :invoice_items, only: %i[edit update]
   end
 
   namespace :admin do
