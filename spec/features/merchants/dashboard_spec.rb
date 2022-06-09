ff299f50d95bd670707a153bf5aede53d4359aa9
Bryce Wein <bryce.wein@gmail.com>
2022-06-09 19:55:33 -0400

    Test: add merchant bulk discounts index test

--- spec/features/merchants/dashboard_spec.rb
+++ spec/features/merchants/dashboard_spec.rb
@@ -119,14 +119,14 @@ RSpec.describe 'Merchant Dashboard Index', type: :feature do
 
     within '#id-0' do
       expect(page).to have_content(@item_2.name)
-      expect(page).to have_link(@invoice_9.id)
+      expect(page).to have_link(@invoice_9.id.to_s)
       expect(page).to have_content('Thursday, Jan 26 2012')
       expect(page).to_not have_content(@item_3.name)
     end
 
     within '#id-1' do
       expect(page).to have_content(@item_1.name)
-      expect(page).to have_link(@invoice_4.id)
+      expect(page).to have_link(@invoice_4.id.to_s)
       expect(page).to have_content('Monday, Mar 26 2012')
       expect(page).to_not have_content('Tuesday, Mar 27 2012')
       expect(@item_1.name).to_not appear_before(@item_2.name)
