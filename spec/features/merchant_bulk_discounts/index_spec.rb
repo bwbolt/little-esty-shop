3361001a2160c447a30abd1bdeea015b59244ad7
Bryce Wein <bryce.wein@gmail.com>
2022-06-09 21:07:58 -0400

    Test: add merchant bulk discount creation test

--- spec/features/merchant_bulk_discounts/index_spec.rb
+++ spec/features/merchant_bulk_discounts/index_spec.rb
@@ -7,6 +7,7 @@ RSpec.describe 'Merchant Bulk Discount Index' do
     @discount2 = BulkDiscount.create!(percentage: 15, threshold: 20, merchant_id: @merchant1.id)
     @discount3 = BulkDiscount.create!(percentage: 30, threshold: 50, merchant_id: @merchant1.id)
   end
+
   it 'should have a link to it from merchants dashboard' do
     visit merchant_dashboard_index_path(@merchant1)
 
@@ -29,5 +30,26 @@ RSpec.describe 'Merchant Bulk Discount Index' do
       expect(page).to have_content(@discount3.threshold)
       expect(page).to have_link('View Discount')
     end
+  end
+
+  it 'should have a link to a working discount creation form' do
+    visit merchant_bulk_discounts_path(@merchant1)
+
+    click_on 'Create New Discount'
+
+    expect(current_path).to eq(new_merchant_bulk_discount_path(@merchant1))
+
+    within '#threshold' do
+      select(100)
+    end
+    within '#percentage' do
+      select(70)
+    end
+
+    click_on 'Create'
+
+    expect(current_path).to eq(merchant_bulk_discounts_path(@merchant1))
+
+    expect(page).to have_content('Buy 100 units, get a 70.0% discount')
   end
 end
